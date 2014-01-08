bba41b91d6c4e5356d207de66921a0d9a7a31a47
Kathryn G Turner <kathryn.g.turner@gmail.com>
2014-01-07 18:43:11 -0800

    pictures for poster: spp tree, map with relevant population points, kmer count plot

--- Asteraceae_genomic_spptree.R
+++ Asteraceae_genomic_spptree.R
@@ -1,6 +1,7 @@
 #phylogeny for alignment
 
-# install.packages("taxize")
+library(devtools)
+install_github("taxize_", "ropensci")
 
 library(taxize)
 library(XML)
@@ -8,7 +9,12 @@ library(reshape2)
 
 # Define a species list
 # 
-spplist <- c("Centaurea diffusa","Artemisia frigida","Jacobaea vulgaris","Ageratina adenophora","Lactuca sativa","Helianthus annuus","Guizotia abyssinica","Parthenium")
+spplist <- c("Centaurea diffusa","Artemisia frigida","Jacobaea vulgaris",
+             "Ageratina adenophora","Lactuca sativa","Helianthus annuus",
+             "Guizotia abyssinica","Parthenium argentatum","Helianthus strumosus",
+             "Helianthus decapetalus","Helianthus grosseserratus", "Helianthus tuberosus",
+             "Helianthus divaricatus", "Helianthus giganteus","Helianthus hirsutus", 
+             "Helianthus maximiliani", "Chrysanthemum indicum", "Chrysanthemum x morifolium")
 # 
 
 # 
@@ -16,8 +22,13 @@ spplist <- c("Centaurea diffusa","Artemisia frigida","Jacobaea vulgaris","Agerat
 # 
 # Plain species tree
 # 
-tree <- phylomatic_tree(taxa = spplist)
+tree <- phylomatic_tree(taxa = spplist, storedtree='smith2011')
 tree$tip.label <- taxize_capwords(tree$tip.label)
-plot(tree, cex=0.75)
 
-legend("bottomleft", legend = c("Weed", "Not a weed"), fill = c("#024DFD", "#ED6500"))
+png("Asteraceae_plastome_tree.png", width=600, height = 600, pointsize = 16)
+
+plot(tree, cex=0.75, main="Published plastomes from the Asteraceae")
+
+dev.off()
+
+# legend("bottomleft", legend = c("Weed", "Not a weed"), fill = c("#024DFD", "#ED6500"))
