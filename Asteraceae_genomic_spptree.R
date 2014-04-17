98374210b653b298a99bab175e79a38453f84738
Kathryn G Turner <kathryn.g.turner@gmail.com>
2014-04-17 16:41:50 -0700

    adj. point size on plastome tree

--- Asteraceae_genomic_spptree.R
+++ Asteraceae_genomic_spptree.R
@@ -32,7 +32,7 @@ tree$tip.label <- taxize_capwords(tree$tip.label)
 # tree$tip.label <- taxize_capwords(tree$tip.label)
 
 
-png("Asteraceae_plastome_treeSmith.png", width=600, height = 600, pointsize = 16)
+png("Asteraceae_plastome_treeSmith.png", width=600, height = 600, pointsize = 12)
 
 plot(tree, cex=1.1, main="Plastomes in the Asteraceae")
 rect(14.8, 15.5, 20.2, 16.4, border="#66CC00",lwd=3)
